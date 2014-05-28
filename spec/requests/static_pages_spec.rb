7fc07cd75af740da43b9b265f7dc401805232f6e
onphenomenon <kariannechristina@gmail.com>
2014-05-28 12:16:15 -0700

    new Contact pg

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  describe "Home page" do
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  	describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -13,7 +15,7 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    	expect(page).to have_title("#{base_title} | Home")
   	end
 end
 
@@ -26,7 +28,7 @@ end
 
 		 it "should have the title 'Help'" do
 		 	visit '/static_pages/help'
-		 	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		 	expect(page).to have_title("#{base_title} | Help")
 		 end
 end
 
@@ -39,7 +41,22 @@ end
 
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+			expect(page).to have_title("#{base_title} | About Us")
 		end
 	end
+
+	describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      
+    end
+
+    it "should have the title 'Contact'" do
+    	visit '/static_pages/contact'
+    	expect(page).to have_title("#{base_title} | Contact")
+  	end
+end
 end
